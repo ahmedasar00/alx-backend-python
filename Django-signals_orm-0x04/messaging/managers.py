4ca1a7a9cdd87a1a7a6fd5cb9d5b7e1e5148079e
AhmedAsar <ahmedasartech@outlook.com>
2025-10-23 20:34:50 +0300

    Modify Manager

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 
-class UnreadMessagesQuerySet(models.QuerySet):
-    def unread_for_user(self, user):
-        return self.filter(receiver=user, read=False)
-
-
 class UnreadMessagesManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(read=False)
 
     def unread_for_user(self, user):
-        return self.get_queryset().filter(receiver=user)
+        return (
+            self.get_queryset()
+            .filter(receiver=user, read=False)
+            .only("id", "sender", "content", "timestamp")
+        )
