3b8df44d619ff80b580a77e73f7ca7f74172d419
AhmedAsar <ahmedasartech@outlook.com>
2025-11-29 20:34:24 +0200

    Fix: correct loging from validation

--- messaging_app/chats/admin.py
+++ messaging_app/chats/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import chatMessage, ChatRoom, User
+
+admin.site.register(User)
+admin.site.register(ChatRoom)
+admin.site.register(chatMessage)
+# class ChatAdmin(admin.ModelAdmin):
+#    list_display = ('id', 'user', 'message', 'timestamp')
+#    search_fields = ('user__username', 'message')
+# admin.site.register(chatMessage, ChatAdmin)
